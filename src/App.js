71f8fa61829eac9dc7e7fbf4739dec64aae10e84
Ilona Ch <chernyavskaya.ilona25@gmail.com>
2022-05-25 17:27:38 +0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React from 'react';
 import MessageItems from './components/MessageItems.js';
-import { useState } from 'react/cjs/react.development';
+import { useEffect, useState } from 'react/cjs/react.development';
 
 const App = () => {
   const [messages, setMessages] = useState([]);
   const [messageInput, setMessageInput] = useState("");
   const [nextId, setNextId] = useState(0);
+  const [intervalIndex, setIntervalIndex] = useState(0);
 
   const sendMessage = (e) => {
     e.preventDefault();
@@ -20,15 +21,35 @@ const App = () => {
   };
 
   const deleteMessage = (messageId) => {
-    setMessages(messages.filter(({ id }) => id !== messageId))
+    setMessages(messages.filter(({ id }) => id !== messageId));
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIntervalIndex(intervalIndex => intervalIndex + 1);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (messages.length <= intervalIndex) {
+      setIntervalIndex(0);
+    }
+  }, [intervalIndex]);
 
   return (
     <div className="App">
-      <div className='setTimeout'>hi</div>
+      {
+        messages.length ?
+          <div className='setTimeout'>
+            <b>{messages[intervalIndex]?.text}</b>
+          </div>
+          :
+          <div className='setTimeout'>Empty</div>
+      }
       <div className='all_messages'>
-        {messages?.map((item) => {
-          return <MessageItems item={item} deleteMessage={deleteMessage} />
+        {messages?.map((item, i) => {
+          return <MessageItems key={i} item={item} deleteMessage={deleteMessage} />
         })}
       </div>
       <form className='input_button' onSubmit={sendMessage}>
@@ -36,7 +57,7 @@ const App = () => {
           value={messageInput}
           onChange={(e) => setMessageInput(e.target.value)}
           name="message" type="text"
-          placeholder="Write a message"
+          placeholder="Write a message.."
         />
         <button onClick={sendMessage}>Send</button>
       </form>
--- src/components/MessageItems.js
+++ src/components/MessageItems.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./MessageItems.css";
 
 const MessageItems = ({item, deleteMessage}) => {
-    
+
     return(
         <div className="messages" key={item.key}>
-                <h5>{item.text}</h5>
-                <span className="close" onClick={() => {deleteMessage(item.id)}}>
-                    X
-                </span>
+            <span className="close" onClick={() => {deleteMessage(item.id)}}>
+                x
+            </span>
+            <h5>{item.text}</h5>        
         </div>
     );
 };
